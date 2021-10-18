3105497b840872548b44f209f27be1189aa93a91
dwedigital <dave@dwedigital.com>
2021-10-18 17:56:14 +0100

    added menu and class file

---
+++ Account.cs
@@ -0,0 +1,6 @@
+namespace Bank{
+
+    class Account{
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace the_banker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How can we help you today? Select from one of the following...");
+            Console.WriteLine("O: Open account");
+            Console.WriteLine("L: List accounts");
+            Console.WriteLine("S: Show single account information");
+            Console.WriteLine("D: Make a deposit");
+            Console.WriteLine("W: Make a withdrawl");
+            Console.WriteLine("T: Make a transfer");
+            string input = Console.ReadLine().ToUpper(); 
+        }
+    }
+}
